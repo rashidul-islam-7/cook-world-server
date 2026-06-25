d4fb35621aad43d33d8a575034f464bdbd994e4a
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-06-25 06:04:46 +0600

    get my recipe and post recipe api added

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -14,46 +13,57 @@ dotenv.config();
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT;
 
-const client = new MongoClient(uri)
+const client = new MongoClient(uri);
 
-app.get('/', (req, res) => {
-  res.send('Hello Developers!');
+app.get("/", (req, res) => {
+  res.send("Hello Developers!");
 });
 
-
-async function run(){
-try{
+async function run() {
+  try {
     const db = client.db("cook-world-data");
     const allRecipeCollection = db.collection("allRecipe");
 
-    // get all recipe data 
-    app.get("/recipes", async (req, res)=>{
-        const result = await allRecipeCollection.find().toArray();
-        res.send(result);
+    // get all recipe data
+    app.get("/recipes", async (req, res) => {
+      const result = await allRecipeCollection.find().toArray();
+      res.send(result);
     });
 
-    // get recipe with id 
-    app.get("/recipes/:id", async (req, res)=>{
-        const id = req.params.id;
-        const result = await allRecipeCollection.findOne({
-            _id: new ObjectId(id)
-        });
-        res.send(result);
+    // get recipe with id
+    app.get("/recipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await allRecipeCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
     });
 
-    // post recipe 
-    app.post("/recipes", async (req, res)=>{
-        const result = await allRecipeCollection.insertOne(req.body);
-        res.send(result);
-    })
-}
-catch(e){
-    console.log(e)
-}
+    // post recipe
+    app.post("/recipes", async (req, res) => {
+      const result = await allRecipeCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    // get my-recipe 
+    app.get("/my-recipes", async (req, res) => {
+      const email = req.query.email;
+
+      const result = await allRecipeCollection
+        .find({ authorEmail: email })
+        .toArray();
+
+      res.send(result);
+    });
+
+
+
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 run();
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
